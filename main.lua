1d19b41d7128ccd85d30d600ff004e9444665ba9
ScriptShark-cmyk <scriptshark24@gmail.com>
2025-06-01 03:36:49 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,16 @@
+getgenv().latis = {
+    reach = {
+        enabled = true,
+        studs = 5,
+        part = "LeftFoot",
+        transparency = 1
+    },
+    react = {
+        enabled = true,
+        kickforce = 3,
+        avgskillprobability = 5
+    }
+}
+
+-- Your actual main script code here
+print("Latis script loaded!")  -- placeholder for real code
