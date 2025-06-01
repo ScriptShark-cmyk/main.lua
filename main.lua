f796e0cded08a52a6ebb17e08e44a4120c47605f
ScriptShark-cmyk <scriptshark24@gmail.com>
2025-06-01 03:42:43 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,16 +1,1 @@
-getgenv().latis = {
-    reach = {
-        enabled = true,
-        studs = 5,
-        part = "LeftFoot",
-        transparency = 1
-    },
-    react = {
-        enabled = true,
-        kickforce = 3,
-        avgskillprobability = 5
-    }
-}
-
--- Your actual main script code here
-print("Latis script loaded!")  -- placeholder for real code
+https://raw.githubusercontent.com/ScriptShark-cmyk/latis-script/main/main.lua
